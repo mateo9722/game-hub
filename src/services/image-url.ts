907835cb3981d323cd7397be1d8d6a8418908ee5
mateo9722 <matemkamelot@hotmail.com>
2024-08-11 22:07:51 -0500

    Get optimized images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -3,3 +3,4 @@ const getCroppedImageUrl = (url: string) => {
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
+export default getCroppedImageUrl;
