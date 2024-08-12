afa87e7e75fb85c8044d2ab0ed5a375014c38c3d
mateo9722 <matemkamelot@hotmail.com>
2024-08-11 23:25:38 -0500

    Create a generic data fetching hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
-import { GenreList } from "./components/GenreList";
+import GenreList from "./components/GenreList";
 function App() {
   return (
     <Grid
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,14 @@
-import React from "react";
 import useGenres from "../hooks/useGenres";
 
-export const GenreList = () => {
-  const { genres } = useGenres();
+const GenreList = () => {
+  const { data } = useGenres();
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </ul>
   );
 };
+
+export default GenreList;
